1689a86f1ea4fe5c2f474085303aade4ba3efa6c
kagel <LuckKage@gmail.com>
2019-12-25 15:24:47 +0300

    Problems with filling of books' table.

--- src/Reader.java
+++ src/Reader.java
@@ -10,20 +10,24 @@ import java.util.*;
 //F:\JavaProjects\LibrusecLibArchive\librusec_local_fb2.inpx
 
 public class Reader {
+//    private static Map<String, Integer> authorsMap = new TreeMap<>();
+    private static Set<String> authorsSet = new TreeSet<>();
+    private static Set<String> booksSet = new TreeSet<>();
+
     public static void main(String[] args) throws SQLException, ParseException {
-//        reader();
-        WorkWithDB.createDB();
+        readFromArchive();
+        WorkWithDB.updateTables(authorsSet, booksSet);
     }
 
-    private static void reader() throws ParseException {
-        Set<String> authorsSet = new TreeSet<>();
-        Map<String, Integer> authorsMap = new TreeMap<>();
-        int count = 0;
+    private static void readFromArchive() {
         String[] buf;
-        String[] subBuf;
+        String[] authorsBuf;
+        String books = "";
         try (ZipFile zipFile = new ZipFile("F:\\JavaProjects\\LibrusecLibArchive\\librusec_local_fb2.inpx");
-             BufferedWriter bw = new BufferedWriter(
-                     new FileWriter("F:\\JavaProjects\\LibrusecLibArchive\\lib.txt"))) {
+             BufferedWriter authorsBw = new BufferedWriter(
+                     new FileWriter("F:\\JavaProjects\\LibrusecLibArchive\\libAuthors.txt"));
+             BufferedWriter booksBw = new BufferedWriter(
+                     new FileWriter("F:\\JavaProjects\\LibrusecLibArchive\\libBooks.txt"))) {
             Enumeration<? extends ZipArchiveEntry> entries = zipFile.getEntries();
             while (entries.hasMoreElements()) {
                 ZipArchiveEntry entry = entries.nextElement();
@@ -33,27 +37,41 @@ public class Reader {
                         while (br.ready()) {
                             buf = (br.readLine().replaceAll("\u0004+", "\u0004")
                                     .replaceAll("\\s+", " ").split("\u0004"));
-                            for (int i = 0; i < buf.length; i++)
-                                buf[i] = buf[i].trim();
-                            if (buf[0].endsWith(":"))
-                                buf[0] = buf[0].substring(0, buf[0].length() - 1);
-                            subBuf = buf[0].split(":");
-                            for (String s : subBuf) {
-                                authorsSet.add(s.replaceAll("[.,]", " ").
-                                        replaceAll("\\s+", " ").
-                                        replaceAll(" :", ":").trim());
+                            if(9 < buf.length) {
+                                for (int i = 0; i < buf.length; i++) {
+                                    buf[i] = buf[i].trim();
+                                    if (i < 2 && (buf[i].endsWith(":")))
+                                        buf[i] = buf[i].substring(0, buf[i].length() - 1);
+                                }
+                                authorsBuf = buf[0].split(":");
+                                for (String s : authorsBuf) {
+                                    authorsSet.add(s.replaceAll("[.,]", " ").
+                                            replaceAll("\\s+", " ").
+                                            replaceAll(" :", ":").trim());
+                                }
+                                books = "\u0004" + buf[2] + "\u0004" + buf[1] + "\u0004" + buf[4]
+                                        + "\u0004" + buf[7] + "\u0004" + buf[8] + "\u0004" + buf[9];
+                                booksSet.add(books.trim());
                             }
                         }
                     }
                 }
             }
-            for(String i : authorsSet) {
+            /*for(String i : authorsSet) {
                 authorsMap.put(i, count++);
             }
             for(Map.Entry<String, Integer> pair : authorsMap.entrySet()) {
                 bw.write(pair.getKey() + " - " + pair.getValue());
                 bw.write("\r\n");
+            }*/
+            for (String i : authorsSet) {
+                authorsBw.write(i);
+                authorsBw.write("\r\n");
             }
+            /*for (String i : booksSet) {
+                booksBw.write(i);
+                booksBw.write("\r\n");
+            }*/
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/WorkWithDB.java
+++ src/WorkWithDB.java
@@ -1,4 +1,8 @@
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Set;
 
 public class WorkWithDB {
     private static Connection dbConnection = null;
@@ -6,77 +10,84 @@ public class WorkWithDB {
 
     private static Connection DBConnection() {
         final String SERVER_ADDRESS = "jdbc:postgresql://localhost/?user=postgres&password=postgres";
+        final String DB_ADDRESS = "jdbc:postgresql://localhost/homelibrary";
+        final String createDB = "CREATE DATABASE homelibrary";
+        final String createAuthorsTable = "CREATE TABLE IF NOT EXISTS authors (ID SERIAL, Author CHAR(150))";
+        final String createBooksTable = "CREATE TABLE IF NOT EXISTS books (ID SERIAL, Title CHAR(100), Genre CHAR(50)," +
+                " № INT, Size INT, Format CHAR(5), Date DATE, Language char(3));";
         Connection dbConnection = null;
         try {
             Class.forName("org.postgresql.Driver");
-            dbConnection = DriverManager.getConnection(SERVER_ADDRESS, "admin", "admin");
-            return dbConnection;
+            dbConnection = DriverManager.getConnection(DB_ADDRESS, "admin", "admin");
         } catch (ClassNotFoundException | SQLException e) {
+            if(e.getMessage().contains("база данных \"homelibrary\" не существует") ||
+                    e.getMessage().contains("database \"homelibrary\" does not exist")) {
+                try {
+                    dbConnection = DriverManager.getConnection(SERVER_ADDRESS, "admin", "admin");
+                    statement = dbConnection.createStatement();
+                    statement.executeUpdate(createDB);
+                    dbConnection = DriverManager.getConnection(DB_ADDRESS, "admin", "admin");
+                } catch (SQLException ex) {
+                    System.err.println(e.getClass().getName() + ": " + ex.getMessage());
+                    ex.printStackTrace();
+                    System.exit(0);
+                }
+            } else {
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }
+        try {
+            statement = dbConnection.createStatement();
+            statement.executeUpdate(createAuthorsTable);
+            statement.executeUpdate(createBooksTable);
+        } catch (SQLException e) {
             e.printStackTrace();
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
         }
         return dbConnection;
     }
 
+    static void updateTables(Set<String> authors, Set<String> books) throws SQLException, ParseException {
+        Calendar today = Calendar.getInstance();
+        long time = today.getTimeInMillis();
+        Date date = new Date(time);
 
-    static void createDB() throws SQLException {
-        final String DB_ADDRESS = "jdbc:postgresql://localhost/homelibrary";
-        final String createDB = "CREATE DATABASE homelibrary";
-        final String createAuthorsTable = "CREATE TABLE IF NOT EXISTS authors (ID SERIAL, Author CHAR(100))";
-        final String createBooksTable = "CREATE TABLE IF NOT EXISTS books (ID SERIAL, Title CHAR(100), Genre CHAR(50)," +
-                " № INT, Size INT, Format CHAR(5), Date DATE, Language char(3));";
+        String[] booksBuf;
         try {
             dbConnection = DBConnection();
-            statement = dbConnection.createStatement();
-            statement.executeUpdate(createDB);
+            PreparedStatement authorsUpdate = dbConnection.prepareStatement(
+                    "INSERT INTO public.authors(author) VALUES (?)");
+            for (String i : authors) {
+                authorsUpdate.setString(1, i);
+                authorsUpdate.executeUpdate();
+            }
+            PreparedStatement booksUpdate = dbConnection.prepareStatement(
+                    "INSERT INTO public.books(title, genre, size, format, date, language) VALUES (?, ?, ?, ?, ?, ?)");
+            for (String i : books) {
+                booksBuf = i.split("\u0004");
+                int k = 1;
+                for(int j = 0; j < booksBuf.length; j++) {
+                    switch (k) {
+                        case 3: booksUpdate.setInt(k, Integer.parseInt(booksBuf[j]));
+                        break;
+                        case 5: booksUpdate.setDate(k, date);
+                        break;
+                        default: booksUpdate.setString(k, booksBuf[j]);
+                        break;
+                    }
+                    k++;
+                }
+                booksUpdate.executeUpdate();
+            }
         } catch (SQLException e) {
-            dbConnection = DriverManager.getConnection(DB_ADDRESS, "admin", "admin");
-            statement = dbConnection.createStatement();
-            statement.executeUpdate(createAuthorsTable);
-            statement.executeUpdate(createBooksTable);
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            e.printStackTrace();
         } finally {
             if (statement != null) statement.close();
             if (dbConnection != null) dbConnection.close();
         }
     }
-
-  /*  static void workWithDB() throws SQLException {
-        String createTable = "CREATE TABLE IF NOT EXISTS HomeLibrary (ID SERIAL, Author CHAR(100), Genre CHAR(50), Title CHAR(100)," +
-                " № INT, Size INT, Format CHAR(5), Date DATE, Language char(3));";
-        String updateTable = "INSERT INTO HomeLibrary (ID, Author, Genre, Title, №, Size, Format, Date, Language)" +
-                "VALUES (" + ")";
-
-        try {
-            dbConnection = DBConnection();
-            statement = dbConnection.createStatement();
-            statement.executeUpdate(createTable);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println(e.getClass().getName() + ": " + e.getMessage());
-        } finally {
-            if (statement != null) statement.close();
-            if (dbConnection != null) dbConnection.close();
-        }
-    }
-
-    static void insertIntoDB(String[] data) throws SQLException, ParseException {
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        String createTable = "CREATE TABLE IF NOT EXISTS HomeLibrary (ID SERIAL, Author CHAR(100), Genre CHAR(50), Title CHAR(100)," +
-                " № INT, Size INT, Format CHAR(5), Date DATE, Language char(3));";
-        String updateTable = "INSERT INTO HomeLibTest (name, genre, title, №, size, format, date, language) " +
-                "VALUES (" + data[0] + ", " + data[1] + ", " + data[2] + ", " + Integer.parseInt(data[3]) + ", " +  Integer.parseInt(data[4]) + ", " + data[7] + ", " + format.parse(data[8]) + ", " + data[9] + ")";
-
-        try {
-            dbConnection = DBConnection();
-            statement = dbConnection.createStatement();
-            statement.executeUpdate(updateTable);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println(e.getClass().getName() + ": " + e.getMessage());
-        } finally {
-            if (statement != null) statement.close();
-            if (dbConnection != null) dbConnection.close();
-        }
-    }*/
 }
+
+
